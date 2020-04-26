f337ae0a059b315bdab3c7d5c9f4aaccbf494dbe
Archie Fielding <archiefielding@gmail.com>
2020-04-26 14:42:36 +0800

    Refactored interactive menu

--- directory.rb
+++ directory.rb
@@ -1,46 +1,71 @@
+@students = [] #an empty array accessible to all methods
+
 def input_students
   puts "Please enter the following for each student."
   puts "To finish, simply hit return twice."
   #create empty array
-  students = []
+#  students = []
   #get the first name
   puts "Name:"
   name = gets.capitalize.chomp
-  #get the country of birth
-  puts "Country of Birth:"
-  cob = gets.capitalize.chomp
-  #while the name or COB is not empty, repeat this code
+  #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :November, cob: cob}
-    puts "Now we have #{students.count} student(s)"
+    @students << {name: name, cohort: :November}
+    puts "Now we have #{@students.count} student(s)"
     #get another name from the user
     puts "Name:"
     name = gets.capitalize.chomp
-    #get the country of birth
-    puts "Country of Birth:"
-    cob = gets.capitalize.chomp
   end
-  #return the array of students
-  students
 end
 
 def print_header
-  puts "The Students of Villains Academy"
-  puts "----------------"
+  puts "|--------------------------------|"
+  puts "|The Students of Villains Academy|"
+  puts "|--------------------------------|"
 end
 
-def print(students)
-  students.each_with_index do |student, index|
-      puts "#{index+1}: #{student[:name]}, #{student[:cob]}, (#{student[:cohort]} cohort)"
+def print_students_list
+  @students.each_with_index do |student, index|
+      puts "#{index+1}: #{student[:name]}, (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students."
+def print_footer
+  puts "Overall, we have #{@students.count} great students."
+  puts "----------------------------------"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def print_menu
+  puts "1. Input the students."
+  puts "2. Show the list of students."
+  puts "9. Exit."
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+interactive_menu
